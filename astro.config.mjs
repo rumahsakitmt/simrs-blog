458b298aa928f765c431fa5392ce33535c5abae5
putuhema <putuhendramahendra@gmail.com>
2025-05-09 14:16:36 +0800

    feat: search with pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import { remarkReadingTime } from './remark-reading-time.mjs';
+import { remarkReadingTime } from "./remark-reading-time.mjs";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: "file",
+  },
   markdown: {
-    remarkPlugins: [remarkReadingTime]
+    remarkPlugins: [remarkReadingTime],
   },
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
   },
